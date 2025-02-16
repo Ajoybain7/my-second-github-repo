9f7484cb3e0b48d379285038f176080bfa778353
Ajoybain7 <ajoybain360@gmail.com>
2025-02-16 17:41:39 +0600

    my second commit

---
+++ ajoy.js
@@ -0,0 +1,8 @@
+const today = new Date();
+const date = new Date('2062-10-19')
+console.log(date.getMonth());
+console.log(date.getDay());
+const specificdate = new Date(2091, 0, 26)
+console.log(specificdate)
+specificdate.setMonth(10);
+console.log(specificdate.toLocaleString('en-GB'));
